c77c1408a8fe7182b1792605daa76ab49fda25e4
Giuliamourac <giuliamoura.carnevale@gmail.com>
2025-03-20 17:06:14 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escreva algo para o Cebolinha te imitar.");
+
+            string frase = Console.ReadLine();
+
+            string cebolinha = frase
+
+                .Replace("R", "L")
+
+                .Replace("r", "l");
+
+            Console.WriteLine($"Cebolinha: {cebolinha}");
+
+        }
+    }
+}
